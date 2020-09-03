2e4bd812655743366bcb1a8269056f1db521cced
Plimon Howard V <plimon.howard@gmail.com>
2020-09-03 20:09:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function happyHolidays(){
 function happyHolidaysTo(string){
     return `Happy holidays, ${string}!`
 }
+
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function happyCustomHolidayTo(days, holiday){
+  return `It's ${days} days until ${holiday}!`
+}
