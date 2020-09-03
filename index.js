a749050ce4e5d123bd273da00beb3a434abf54fa
Plimon Howard V <plimon.howard@gmail.com>
2020-09-03 20:02:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function happyHolidays(){
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(name){
-    return "Happy holidays, ${name}!"
+function happyHolidaysTo(string){
+    return "Happy holidays, ${string}!"
 }
 
 console.log(happyHolidaysTo("you"))
